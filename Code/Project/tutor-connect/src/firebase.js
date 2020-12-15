7049ed90f8d75ccbc0059a6fea5aa1d0112f8afe
Samiya Kazi <samiya.kazi@northsouth.edu>
2020-12-15 18:37:26 +0600

    Enhancement #6: Intialized Firebase
    Intialized Firebase and added configuration details.

---
+++ Code/Project/tutor-connect/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBf-gmLfs9G8-ISYtEVchVk9YB2FNHyOv8",
+    authDomain: "tutor-connect-1b25b.firebaseapp.com",
+    projectId: "tutor-connect-1b25b",
+    storageBucket: "tutor-connect-1b25b.appspot.com",
+    messagingSenderId: "1025195952610",
+    appId: "1:1025195952610:web:a7a84d51e947e3677311bb",
+    measurementId: "G-RGBWN0F3RN"
+  };
+
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
