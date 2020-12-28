09f259625e60125b5663f0f4cf3b0d7d31f48232
Samiya Kazi <samiya.kazi@northsouth.edu>
2020-12-28 18:15:37 +0600

    Enhancement #12: Edited Material UI imports
    Edited Material UI imports needed for form.

--- Code/Project/tutor-connect/src/components/NewOfferForm.js
+++ Code/Project/tutor-connect/src/components/NewOfferForm.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
 
 class NewOfferForm extends Component {
