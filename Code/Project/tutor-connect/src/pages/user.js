b556d262aeeea08e3fe70c31ace44a2d72ba8377
Samiya Kazi <samiya.kazi@northsouth.edu>
2021-01-05 18:19:22 +0600

    Enhancement #16: Added user posts to profile
    Added user posts to profile

--- Code/Project/tutor-connect/src/pages/user.js
+++ Code/Project/tutor-connect/src/pages/user.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import firebaseApp from '../firebase';
+import OfferPost from '../components/OfferPost';
+
+//Material UI Components
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
+const db = firebaseApp.firestore();
+
 class User extends Component {
 
+	constructor(props){
+		super(props);
+
+		this.state = {
+			userID: this.props.match.params.userID,
+			userInfo: {},
+			offers: []
+		}
+	}
+
+
+	//Fetch user offers from Firestore
+	async getUserOffers() {
+		await db.collection("offers").where("userID","==", this.state.userID).get()
+			.then((querySnapshot) => {
+                const offersSet = querySnapshot.docs.map((doc) => {
+                    const data = doc.data();
+                    data.offerID = doc.id;
+                    return data;
+                });
+                this.setState({offers: offersSet});
+			})
+			.catch(err => console.log(err));
+
+	}
+
 	render() {
+		this.getUserOffers();
+        let userOffers = this.state.offers.map((offer) => {if (offer) { 
+                return (<OfferPost key={offer.offerID} offer={offer} currentUser={this.state.user} />);}
+            });
+
 		return (
 			<Grid container>
 				<Grid item sm={6} xs={12}>
@@ -21,8 +58,10 @@ class User extends Component {
                         </CardContent>
                     </Card>
 				</Grid>
-				<Grid item sm={6} xs={12}>
-
+				<Grid item sm={1} xs={12}></Grid>
+				<Grid item sm={5} xs={12}>
+					<h1>Offers Posted</h1>
+					<div>{userOffers}</div>
 				</Grid>
 			</Grid>
 		);
